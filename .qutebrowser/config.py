02049dd6f3ae9a03b056bf88f63b7850a7b74f9e
johngodlee <johngodlee@gmail.com>
2018-07-18 12:14:20 +0100

    Fixed qutebrowser config, giving it another go!

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -395,10 +395,25 @@ c.fonts.statusbar = '8pt monospace'
 # Type: QtFont
 c.fonts.tabs = '9pt monospace'
 
-# Bindings for normal mode
+# indings for normal mode
 config.bind('<backspace>', 'back')
 config.bind('gT', 'tab-prev')
 config.bind('gt', 'tab-next')
 config.bind('q', None)
 config.bind('qq', 'quit')
 config.bind('<Meta-R>', 'reload')
+
+# Definitions of search engines which can be used via the address bar.
+# Maps a searchengine name (such as `DEFAULT`, or `ddg`) to a URL with a
+# `{}` placeholder. The placeholder will be replaced by the search term,
+# use `{{` and `}}` for literal `{`/`}` signs. The searchengine named
+# `DEFAULT` is used when `url.auto_search` is turned on and something
+# else than a URL was entered to be opened. Other search engines can be
+# used by prepending the search engine name to the search term, e.g.
+# `:open google qutebrowser`.
+# Type: Dict
+c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}', 'google': 'https://www.google.com/search?hl=en&q={}', 'reddit': 'https://www.reddit.com/search?q={}', 'stack': 'https://stackexchange.com/search?q={}', 'youtube': 'https://www.youtube.com/results?search_query={}', 'hn': 'https://hn.algolia.com/?query={}'}
+
+# The page(s) to open at the start.
+# Type: List of FuzzyUrl, or FuzzyUrl
+c.url.start_pages = '~/.qutebrowser/homepage/home.html'
