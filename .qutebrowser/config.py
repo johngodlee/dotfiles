e9ee384d925239a8313c00e2e46c7df4a67473ad
johngodlee <johngodlee@gmail.com>
2018-07-31 14:04:50 +0100

    Titles are better now

--- .qutebrowser/config.py
+++ .qutebrowser/config.py
@@ -412,7 +412,7 @@ config.bind('<Meta-R>', 'reload')
 # used by prepending the search engine name to the search term, e.g.
 # `:open google qutebrowser`.
 # Type: Dict
-c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}', 'google': 'https://www.google.com/search?hl=en&q={}', 'reddit': 'https://www.reddit.com/search?q={}', 'stack': 'https://stackexchange.com/search?q={}', 'youtube': 'https://www.youtube.com/results?search_query={}', 'hn': 'https://hn.algolia.com/?query={}'}
+c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}', 'google': 'https://www.google.com/search?hl=en&q={}', 'reddit': 'https://www.reddit.com/search?q={}', 'stack': 'https://stackexchange.com/search?q={}', 'youtube': 'https://www.youtube.com/results?search_query={}', 'hn': 'https://hn.algolia.com/?query={}', 'scholar': 'https://scholar.google.co.uk/scholar?hl=en&as_sdt=0%2C5&q={}.&btnG='}
 
 # The page(s) to open at the start.
 # Type: List of FuzzyUrl, or FuzzyUrl
