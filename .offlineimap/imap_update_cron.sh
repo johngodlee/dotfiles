20e19697815a33a9c591b2309abaec2b1d72b3ae
johngodlee <johngodlee@gmail.com>
2018-09-02 07:50:17 +0100

    Added new imap update script for cron

---
+++ .offlineimap/imap_update_cron.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+imapserver=$(cat ~/.offlineimaprc | grep remotehost | awk '{print $3}')
+
+imapactive=$(ps -ef | grep '[o]fflineimap' | wc -l)
+
+netactive=$(ping -c3 $imapserver >/dev/null 2>&1 && echo up || echo down)
+
+mailsync="/usr/local/bin/offlineimap -u quiet -q"
+
+# kill offlineimap if active, sometimes it hangs
+case $imapactive in
+'1')
+    killall offlineimap && sleep 5
+;;
+esac
+
+# Check that you can access the SMTP server
+case $netactive in
+'up')
+    $mailsync
+;;
+'down')
+    :
+;;
+esac
