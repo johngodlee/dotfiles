63b52ad96f719afbe83325cff3078a90e0fb97fa
johngodlee <johngodlee@gmail.com>
2018-11-28 09:11:55 +0000

    Made changes to offlineimap setup to include riseup

--- .offlineimap.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-
-from subprocess import check_output
-
-def get_pass():
-    return check_output("/usr/local/bin/pass email/johngodlee@gmail.com", shell=True).splitlines()[0]
-
-
