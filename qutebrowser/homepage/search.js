b2a8d858fe19dbf6f219c044b7494bed421bea27
johngodlee <johngodlee@gmail.com>
2019-10-13 21:59:31 +0100

    Tidied stow

--- qutebrowser/homepage/search.js
+++
@@ -1,8 +0,0 @@
-// Search multiple search engines from one box with a dropdown menu
-
-function dosearch() {
-var sf=document.searchform;
-var submitto = sf.sengines.options[sf.sengines.selectedIndex].value + escape(sf.searchterms.value);
-window.location.href = submitto;
-return false;
-}
